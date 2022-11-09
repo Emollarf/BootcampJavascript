5cd9dfc6ee925a87b72330d936317d106e56896f
Emollarf <elena.mollar@hotmail.com>
2022-11-09 09:49:17 +0100

    Variables

---
+++ Modulo-1/ejercicio-2/variables.js
@@ -0,0 +1,11 @@
+const Nombre = "Elena";
+const Edad = 32;
+const mayorDeEdad = true;
+const direccion = { 
+    calle: "Falsa", 
+    población: "Pais de las maravillas", 
+    codigoPostal: 12345}
+const coloresFavoritos = [ "azul", "morado", "verde" ]
+const lenguajesFavoritos = [ "javaScript", "python", "C" ]
+const mejorLenguaje = lenguajesFavoritos [0]
+const peorLenguaje = lenguajesFavoritos [2]
