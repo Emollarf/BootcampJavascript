084c823e87007e3f09512a2c71facf48d82a420b
Emollarf <elena.mollar@hotmail.com>
2022-11-09 11:06:42 +0100

    operaciones

---
+++ Modulo-1/ejercicio-3/variables-numeros.js
@@ -0,0 +1,17 @@
+let numero1 = 8
+let numero2 = 3
+let total = numero1 + numero2
+console.log (total)
+
+let numero3 = 10
+let numero4 = 4
+console.log (numero4 * numero3)
+
+let num1 = 5
+let num2 = 7
+console.log (num2 - num1)
+
+let num3 = 9
+let num4 = 3
+console.log (num3 / num4)
+
