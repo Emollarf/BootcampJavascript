09a92c7872fc4a55408dd1a4051c5bd06d6a62c7
Emollarf <elena.mollar@hotmail.com>
2022-11-09 17:20:09 +0100

    bucles y arrays con objetos

--- Modulo-1/ejercicio-5/pokemon.js
+++ Modulo-1/ejercicio-5/pokemon.js
@@ -1,6 +1,25 @@
 const pokemon = ["Picachu", "Bulbasur", "Charmander",
 ]
 console.log (pokemon)
-console.log (pokemon[0], pokemon[1], pokemon[2])
 
-for (const element of pokemon) {console.log(element)}
+// mostrar los nombres de los pokemons en linas diferentes
+console.log (pokemon[0])
+console.log (pokemon[1])
+console.log (pokemon[2])     
+
+// mostrar los nomnbres de los pokemons en la misma linea
+console.log (pokemon[0], pokemon[1], pokemon[2])
+console.log (pokemons.join (" ")) // otra manera de mostrar cada nombre de pokemon en una linea.
+
+
+// Diferentes tipos de bucles
+for (const element of pokemon) {
+    console.log(element)}
+
+for(let i=0; i<pokemon.leght-1; i++){
+    console.log(pokemon[i])
+}
+
+pokemon.forEach((element) => {
+    console.log (element);
+});
