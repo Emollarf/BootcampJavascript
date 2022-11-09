8cbb8c06a7b1da84d3f9a3e226b7196e2cd76ff8
Emollarf <elena.mollar@hotmail.com>
2022-11-09 11:11:00 +0100

    pokemon

---
+++ Modulo-1/ejercicio-5/pokemon.js
@@ -0,0 +1,6 @@
+const pokemon = ["Picachu", "Bulbasur", "Charmander",
+]
+console.log (pokemon)
+console.log (pokemon[0], pokemon[1], pokemon[2])
+
+for (const element of pokemon) {console.log(element)}
