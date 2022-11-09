5bde41c1fe729fdca744dd27eb66730ee6c639c6
Emollarf <elena.mollar@hotmail.com>
2022-11-09 15:24:27 +0100

    var + bucles + comentarios

--- Modulo-1/ejercicio-6/ejercicio-6.js
+++ Modulo-1/ejercicio-6/ejercicio-6.js
@@ -1,4 +1,4 @@
-const pokemons = [
+const pokemons = [  // Cuando es un array de objetos, primero se crea el array y luego se escriben las propiedades de los objetos. 
 {
     nombre: "Charmander",
     tipoDePokemon: "Fuego"
@@ -14,19 +14,19 @@ const pokemons = [
 ] 
      console.log(pokemons)
 
-for (const pokemon of pokemons) { 
-    if (pokemon.tipoDePokemon == "Fuego") {
+for (const pokemon of pokemons) { // pokemon es un elemento de la lista pokemons
+    if (pokemon.tipoDePokemon == "Fuego") { //se coge un elemento de la lista y la clase determinada, no la lista.
         console.log( "Es un pokemon de fuego")
     }
     else {  
         console.log ("no es un pokemon de fuego")
-    } //pokemon es un elemento de la lista pokemons
+    } 
     
 }
 
 for (const pokemon of pokemons) {
     if (pokemon.tipoDePokemon == "Fuego"){
-        console.log (pokemon.nombre + " " + "Es un pokemon de fuego")
+        console.log (pokemon.nombre + " " + "Es un pokemon de fuego") // los strings hay que + para que se imprima todo
     }
     else if ( pokemon.tipoDePokemon == "Planta"){
         console.log (pokemon.nombre + " " + "Es un pokemon de planta")
