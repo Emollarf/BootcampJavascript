b1661b9ee9b995fd99bc922d4b2efeaab3c452b9
Emollarf <elena.mollar@hotmail.com>
2023-01-02 14:42:11 +0100

    ejercicio 2.1

---
+++ Modulo-2/ejercicio 2/ejercicio 2.1.js
@@ -0,0 +1,17 @@
+const app = document.querySelector(".app");
+const img = document.createElement ('img');
+const web = 'https://via.placeholder.com/150'
+img.src = web
+img.alt = 'imagen ejemplo'
+
+app.append(img)
+
+
+const parrafo = document.querySelector('p')
+/* parrafo.append(img) */
+/* 
+app.insertBefore(img, parrafo) */
+
+
+const parrafo2 = document.querySelector('p:nth-child(2)')
+app.insertBefore(img, parrafo2)
